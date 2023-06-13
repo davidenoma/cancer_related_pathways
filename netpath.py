34ead0c14a330a5a2df547b84d1b6dccadec88cd
David Enoma <davidenoma@gmail.com>
2023-06-13 15:32:19 -0600

    first commit

--- netpath.py
+++ netpath.py
@@ -7,19 +7,19 @@ gene_name = "CXCL3"
 # Define the file path of the TSV file
 net_path_geneset = path_to_gene_set + "/NetPath_Gene_regulation_all.txt"
 
-def find_pathway(gene_name):
+def get_pathway_name(gene_name):
     # Read the TSV file
-    with open(net_path_geneset, "r") as file:
-        lines = file.readlines()
-        header = lines[0].strip().split("\t")  # Get the header row and split by tab delimiter
-        gene_index = header.index("Gene name")  # Find the index of the "Gene name" column
+    with open(net_path_geneset, 'r') as file:
+        for line in file:
+            columns = line.strip().split('\t')
+            if columns[3] == gene_name:
+                return columns[1]  # Pathway name is in column two
+    return None
 
-        for line in lines[1:]:  # Skip the header row
-            row = line.strip().split("\t")  # Split the line by tab delimiter
-            if row[gene_index] == gene_name:
-                pathway_name = row[1]  # Get the pathway name (column 2)
-                print("Pathway Name:", pathway_name)
-                break  # Stop searching after finding the first match
-        else:
-            print("Gene name not found in the TSV file.")
+get_pathway_name("CXCL1")
 
+pathway_name = get_pathway_name(gene_name)
+if pathway_name:
+    print(f"Pathway name for {gene_name}: {pathway_name}")
+else:
+    print(f"Gene name {gene_name} not found in the file.")
